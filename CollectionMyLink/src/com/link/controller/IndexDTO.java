59be2494475f1576c8f4227d19d1dbcd89f036da
NyeongB <kdtoaj14@ajou.ac.kr>
2020-08-07 17:20:32 +0900

    add IndexDTO, Paging.java for paging process

---
+++ CollectionMyLink/src/com/link/controller/IndexDTO.java
@@ -0,0 +1,40 @@
+package com.link.controller;
+
+public class IndexDTO
+{
+	private int start, end;
+	
+	private String b_user_code;
+
+	public String getB_user_code()
+	{
+		return b_user_code;
+	}
+
+	public void setB_user_code(String b_user_code)
+	{
+		this.b_user_code = b_user_code;
+	}
+
+	public int getStart()
+	{
+		return start;
+	}
+
+	public void setStart(int start)
+	{
+		this.start = start;
+	}
+
+	public int getEnd()
+	{
+		return end;
+	}
+
+	public void setEnd(int end)
+	{
+		this.end = end;
+	}
+	
+	
+}
